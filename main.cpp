36b22b3a625f84837e466cec8c04b7ee229b6e97
Eegalmc2 <147062764+Eegalmc2@users.noreply.github.com>
2023-12-17 22:01:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,22 +12,23 @@ Input input;
 sf::Font font;
 
 sf::Text txt;
+sf::Text txt_fps;
 
 bool mode;
 
-float posBatLeftX    = 50;
-float posBatRightX   = WIN_WIDTH - 70;
-float posBatLeftY    = WIN_HEIGHT / 2;
-float posBatRightY   = posBatLeftY;
-float batSpeed       = 0.1f;
-int   batHeight      = 150;
-int   batWidth       = 20;
+int posBatLeftX      = 50;
+int posBatRightX     = WIN_WIDTH - 70;
+int posBatLeftY      = WIN_HEIGHT / 2;
+int posBatRightY     = posBatLeftY;
+int batSpeed         = 13;
+int batHeight        = 150;
+int batWidth         = 20;
 
-float ballSpeed      = 0.01f;
-sf::Vector2f ballDir = sf::Vector2f(1.5f, 2.0f);
-float ballPosX       = WIN_WIDTH / 2.0f;
-float ballPosY       = WIN_HEIGHT / 2.0f;
-float ballHeight     = 15.0f;
+int ballSpeed        = 3;
+sf::Vector2f ballDir = sf::Vector2f(1, 2);
+int ballPosX         = WIN_WIDTH / 2;
+int ballPosY         = WIN_HEIGHT / 2;
+int ballHeight       = 15;
 
 int scoreJ1          = 0;
 int scoreJ2          = 0;
@@ -54,13 +55,16 @@ static void SetText(sf::Text& txt, sf::String str, int x, int y, int height)
 static void menu()
 {
 	bool is_mode_taked = false;
-	sf::Text mtxt_T, mtxt_A;
+	sf::Text mtxt_T, mtxt_A, title;
 
-	SetText(mtxt_T, "Choose a mode: \n", WIN_WIDTH / 2 - 200, 10, 40);
+	SetText(title, "Pong Game !", WIN_WIDTH / 2 - 200, 20, 50);
+
+	SetText(mtxt_T, "Choose a mode: \n", WIN_WIDTH / 2 - 200, 150, 40);
 	mtxt_T.setOutlineThickness(1.0f);
-	SetText(mtxt_A, "\tA: you vs IA\n\tZ: you vs you", WIN_WIDTH / 2 - 200, 60, 35);
+	SetText(mtxt_A, "\tA: you vs IA\n\tZ: you vs you", WIN_WIDTH / 2 - 200, 200, 35);
 
 	window.clear(sf::Color::Black);
+	window.draw(title);
 	window.draw(mtxt_T);
 	window.draw(mtxt_A);
 	window.display();
@@ -92,13 +96,13 @@ static void menu()
 	}
 
 	posBatLeftX = 50;
-	posBatRightX = WIN_WIDTH - 70.0f;
-	posBatLeftY = WIN_HEIGHT / 2.0f;
+	posBatRightX = WIN_WIDTH - 70;
+	posBatLeftY = WIN_HEIGHT / 2;
 	posBatRightY = posBatLeftY;
 
-	sf::Vector2f ballDir = sf::Vector2f(1.5f, 2.0f);
-	ballPosX = WIN_WIDTH / 2.0f;
-	ballPosY = WIN_HEIGHT / 2.0f;
+	sf::Vector2f ballDir = sf::Vector2f(1, 2);
+	ballPosX = WIN_WIDTH / 2;
+	ballPosY = WIN_HEIGHT / 2;
 
 	scoreJ1 = 0;
 	scoreJ2 = 0;
@@ -109,28 +113,46 @@ static void CheckBtn()
 {
 	if (input.getButton().up == true)
 	{
+		if (mode)
+		{
+			posBatLeftY -= batSpeed;
+			if (posBatLeftY < 0)
+				posBatLeftY = 0;
+		}
+		else
+		{
+			posBatRightY -= batSpeed;
+			if (posBatRightY < 0)
+				posBatRightY = 0;
+		}
+	}
+	if (input.getButton().down == true)
+	{
+		if (mode)
+		{
+			posBatLeftY += batSpeed;
+			if (posBatLeftY + batHeight > WIN_HEIGHT)
+				posBatLeftY = WIN_HEIGHT - batHeight;
+		}
+		else
+		{
+			posBatRightY += batSpeed;
+			if (posBatRightY + batHeight > WIN_HEIGHT)
+				posBatRightY = WIN_HEIGHT - batHeight;
+		}
+	}
+
+	if (!mode && input.getButton().A == true)
+	{
 		posBatLeftY -= batSpeed;
 		if (posBatLeftY < 0)
 			posBatLeftY = 0;
 	}
-	if (input.getButton().down == true)
+	if (!mode && input.getButton().Q == true)
 	{
 		posBatLeftY += batSpeed;
 		if (posBatLeftY + batHeight > WIN_HEIGHT)
 			posBatLeftY = WIN_HEIGHT - batHeight;
-	}
-
-	if (input.getButton().left == true && !mode)
-	{
-		posBatRightY -= batSpeed;
-		if (posBatRightY < 0)
-			posBatRightY = 0;
-	}
-	if (input.getButton().right == true && !mode)
-	{
-		posBatRightY += batSpeed;
-		if (posBatRightY + batHeight > WIN_HEIGHT)
-			posBatRightY = WIN_HEIGHT - batHeight;
 	}
 
 	if (input.getButton().escape == true)
@@ -162,8 +184,8 @@ static void UpdateBall()
 	if (ballPosX < 0)
 	{
 		scoreJ2++;
-		ballPosX = WIN_WIDTH / 2.0f;
-		ballPosY = WIN_HEIGHT / 2.0f;
+		ballPosX = WIN_WIDTH / 2;
+		ballPosY = WIN_HEIGHT / 2;
 		ballDir.x = fabs(ballDir.x);
 		ballDir.y *= -1;
 		SetText(txt, std::to_string(scoreJ1) + " | " + std::to_string(scoreJ2));
@@ -173,15 +195,15 @@ static void UpdateBall()
 	if (ballPosX > WIN_WIDTH)
 	{
 		scoreJ1++;
-		ballPosX = WIN_WIDTH / 2.0f;
-		ballPosY = WIN_HEIGHT / 2.0f;
+		ballPosX = WIN_WIDTH / 2;
+		ballPosY = WIN_HEIGHT / 2;
 		ballDir.x = -fabs(ballDir.x);
 		ballDir.y *= -1;
 		SetText(txt, std::to_string(scoreJ1) + " | " + std::to_string(scoreJ2));
 	}
 
 	// top and botton wall
-	if (ballPosY < 0 || ballPosY > WIN_HEIGHT)
+	if (ballPosY < 0 || ballPosY > WIN_HEIGHT - ballHeight)
 	{
 		ballDir.y *= -1;
 	}
@@ -189,7 +211,7 @@ static void UpdateBall()
 
 static void AIBat()
 {
-	posBatRightY = ballPosY - batHeight / 2.0f;
+	posBatRightY = ballPosY - batHeight / 2;
 	if (posBatRightY < 0)
 		posBatRightY = 0;
 	if (posBatRightY + batHeight > WIN_HEIGHT)
@@ -199,6 +221,7 @@ static void AIBat()
 int main()
 {
 	window.create(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT, 32), "Pong SFML c++", sf::Style::Default);
+	window.setFramerateLimit(60);
 
 	font.loadFromFile("resource/Silkscreen-Regular.ttf");
 
@@ -215,8 +238,14 @@ int main()
 	sf::RectangleShape rectangleShape2(sf::Vector2f(batWidth, batHeight));
 	rectangleShape2.setPosition(posBatRightX, posBatRightY);
 
+	sf::Clock clock;
+
 	while (window.isOpen())
 	{
+		int fps = 1 / clock.getElapsedTime().asSeconds();
+		clock.restart();
+		SetText(txt_fps, "fps: " + std::to_string(fps), WIN_WIDTH - 75, 10, 15);
+
 		sf::Event event;
 
 		while (window.pollEvent(event))
@@ -239,6 +268,7 @@ int main()
 		window.clear(sf::Color::Black);
 
 		window.draw(txt);
+		window.draw(txt_fps);
 		window.draw(circleShape);
 		window.draw(rectangleShape);
 		window.draw(rectangleShape2);
